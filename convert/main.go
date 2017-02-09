178069c45a708a271f85640ebc8cfd7320902d0d
maxmcd <max.t.mcdonnell@gmail.com>
2017-02-09 15:02:21 -0500

    working, with bugs

--- convert/main.go
+++ convert/main.go
@@ -3,16 +3,15 @@ package main
 import (
 	"fmt"
 	"io"
+	"log"
 	"mime"
 	"mime/multipart"
 	"net/http"
 	"os"
 	"os/exec"
-	"time"
 
 	"github.com/gorilla/handlers"
 	"github.com/gorilla/mux"
-	"github.com/kr/pty"
 )
 
 func main() {
@@ -21,30 +20,9 @@ func main() {
 	http.ListenAndServe(":8080", handlers.LoggingHandler(os.Stdout, r))
 }
 
-func writeCmdOutput(res http.ResponseWriter, pipeReader *io.PipeReader) {
-	buffer := make([]byte, 1024)
-	for {
-		n, err := pipeReader.Read(buffer)
-		if err != nil {
-			// pipeReader.Close()
-			fmt.Println(err)
-			break
-		}
-
-		data := buffer[0:n]
-		res.Write(data)
-		// if f, ok := res.(http.Flusher); ok {
-		// 	f.Flush()
-		// }
-		//reset buffer
-		for i := 0; i < n; i++ {
-			buffer[i] = 0
-		}
-	}
-}
-
 func indexHandler(w http.ResponseWriter, req *http.Request) {
 	mediaType, params, err := mime.ParseMediaType(req.Header.Get("Content-Type"))
+	_ = mediaType
 	if err != nil {
 		fmt.Println(err)
 	}
@@ -52,46 +30,49 @@ func indexHandler(w http.ResponseWriter, req *http.Request) {
 
 	// cmd := exec.Command("unoconv", "-f", "pdf", "--stdin", "--stdout")
 	cmd := exec.Command("cat")
+
 	stdin, err := cmd.StdinPipe()
 	if err != nil {
 		panic(err)
 	}
 
-	f, err := pty.Start(cmd)
+	stdout, err := cmd.StdoutPipe()
 	if err != nil {
+		log.Fatal(err)
+	}
+
+	if err := cmd.Start(); err != nil {
 		panic(err)
 	}
 
-	go func() {
-		// if err := cmd.Start(); err != nil {
-		// 	panic(err)
-		// }
+	var filename string
 
+	go func() {
 		for {
-			p, err := mr.NextPart()
+			part, err := mr.NextPart()
 			if err == io.EOF {
+				stdin.Close()
 				break
 			}
 			if err != nil {
-				panic(err)
+				// panic(err)
+				fmt.Println(err)
+				break
 			}
-
-			io.Copy(stdin, p)
-			fmt.Printf("%#v\n", p.Header)
-			// w.Write(p)
-			// slurp, err := ioutil.ReadAll(p)
-			// if err != nil {
-			//  log.Fatal(err)
-			// }
-		}
-
-		// io.Copy(w, stdout)
-		if err := cmd.Wait(); err != nil {
-			panic(err)
+			if part.FormName() == "file" {
+				filename = part.FileName() + ".pdf"
+				w.Header().Set("Content-Disposition", fmt.Sprintf("attachment; filename=%s", filename))
+				io.Copy(stdin, part)
+			}
 		}
 	}()
-	io.Copy(w, f)
-	time.Sleep(time.Second * 3)
 
-	_, _ = mr, mediaType
+	io.Copy(w, stdout)
+
+	if err := cmd.Wait(); err != nil {
+		panic(err)
+	}
+	fmt.Println(filename)
+
+	// _, _ = mr, mediaType
 }
