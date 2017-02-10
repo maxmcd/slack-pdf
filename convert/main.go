57ed458a306c48e0dc3ffdbd18f70493cd90d08a
maxmcd <max.t.mcdonnell@gmail.com>
2017-02-09 19:18:41 -0500

    still confused

--- convert/main.go
+++ convert/main.go
@@ -4,8 +4,6 @@ import (
 	"fmt"
 	"io"
 	"log"
-	"mime"
-	"mime/multipart"
 	"net/http"
 	"os"
 	"os/exec"
@@ -17,16 +15,21 @@ import (
 func main() {
 	r := mux.NewRouter()
 	r.HandleFunc("/", indexHandler).Methods("POST")
+	fmt.Println("I'm up")
 	http.ListenAndServe(":8080", handlers.LoggingHandler(os.Stdout, r))
 }
 
 func indexHandler(w http.ResponseWriter, req *http.Request) {
-	mediaType, params, err := mime.ParseMediaType(req.Header.Get("Content-Type"))
-	_ = mediaType
-	if err != nil {
-		fmt.Println(err)
-	}
-	mr := multipart.NewReader(req.Body, params["boundary"])
+	// mediaType, params, err := mime.ParseMediaType(req.Header.Get("Content-Type"))
+	// _ = mediaType
+	// if err != nil {
+	// 	fmt.Println(err)
+	// }
+	// mr := multipart.NewReader(req.Body, params["boundary"])
+	var filename string
+	file, header, err := req.FormFile("file")
+	// fmt.Println(header)
+	_ = header
 
 	// cmd := exec.Command("unoconv", "-f", "pdf", "--stdin", "--stdout")
 	cmd := exec.Command("cat")
@@ -35,6 +38,7 @@ func indexHandler(w http.ResponseWriter, req *http.Request) {
 	if err != nil {
 		panic(err)
 	}
+	defer stdin.Close()
 
 	stdout, err := cmd.StdoutPipe()
 	if err != nil {
@@ -44,30 +48,21 @@ func indexHandler(w http.ResponseWriter, req *http.Request) {
 	if err := cmd.Start(); err != nil {
 		panic(err)
 	}
-
-	var filename string
-
+	fmt.Println("io.copy")
 	go func() {
-		for {
-			part, err := mr.NextPart()
-			if err == io.EOF {
-				stdin.Close()
-				break
-			}
-			if err != nil {
-				// panic(err)
-				fmt.Println(err)
-				break
-			}
-			if part.FormName() == "file" {
-				filename = part.FileName() + ".pdf"
-				w.Header().Set("Content-Disposition", fmt.Sprintf("attachment; filename=%s", filename))
-				io.Copy(stdin, part)
-			}
+		_, err := io.Copy(stdin, file)
+		if err != nil {
+			panic(err)
 		}
 	}()
 
-	io.Copy(w, stdout)
+	fmt.Println("initate copy to sdtout")
+	_, err = io.Copy(w, stdout)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("yo")
+	fmt.Printf("%#v\n", cmd.ProcessState)
 
 	if err := cmd.Wait(); err != nil {
 		panic(err)
